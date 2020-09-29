a1e2b551c3d9361d3995b8dc33bf05c37d79adda
github username <github email address>
2020-09-28 17:06:06 -0700

    css is ok. Not the best designer lol. moving on

--- src/components/browseComponents/CategoryCard.js
+++ src/components/browseComponents/CategoryCard.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import Card from 'react-bootstrap/Card'
 function CategoryCard(props) {
     console.log("info card props: ", props)
     return (
-        <Card className="bg-dark text-white" id="itemCard">
-            <Card.Img src={props.category.img} alt="Card image" className="image" />
-            <Card.ImgOverlay className="cardBody">
-                <Card.Title className="catCardTitle">{props.category.name}</Card.Title>
-            </Card.ImgOverlay>
-        </Card>
+        <NavLink to={`/browse/categories/${props.category.name}`} className="itemCard">
+            <Card className="bg-dark text-white">
+                <Card.Img src="https://tedideas.files.wordpress.com/2020/06/final_booklist_black-writers.jpg?w=750" alt="Card image" className="image" />
+                <Card.ImgOverlay className="cardText">
+                    <Card.Title className="catCardTitle">{props.category.name}</Card.Title>
+                </Card.ImgOverlay>
+            </Card>
+
+        </NavLink>
     )
 }
 
