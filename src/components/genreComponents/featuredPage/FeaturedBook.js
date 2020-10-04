ce68d7991bff838d1b78305bb47ab530cd7ead66
github username <github email address>
2020-10-04 11:11:23 -0700

    Modal
    Modal is able to mount on click and unmount on click. Need to structure and style the content then all done!

--- src/components/genreComponents/featuredPage/FeaturedBook.js
+++ src/components/genreComponents/featuredPage/FeaturedBook.js
@@ -1,13 +1,37 @@
 import React from 'react'
 
-function FeaturedBook({ book }) {
-    return (
-        //Takes the book and prints to DOM
-        <div className="genre-card" key={book.title}>
-            <img alt="book" className="book-img" src={book.img} />
-            <h3 className="book-title">{book.title}</h3>
-        </div>
-    )
+class FeaturedBook extends React.Component {
+    state = {
+        display: "none"
+    }
+    showModal = () => {
+        this.setState(() => {
+            return { display: "block" }
+        })
+    }
+    hideModal = () => {
+        this.setState(() => {
+            return { display: "none" }
+        })
+    }
+    render() {
+        let { book } = this.props
+        return (
+            <>
+                <div className="genre-card" key={book.title} onClick={this.showModal}>
+                    <img alt="book" className="book-img" src={book.img} />
+                    <h3 className="book-title">{book.title}</h3>
+                </div>
+                <div className="modal" style={{ display: this.state.display }}>
+                    <div className="modal-content">
+                        <span className="close" onClick={this.hideModal}>&times;</span>
+                        <p>Some text in the Modal..</p>
+                    </div>
+
+                </div>
+            </>
+        )
+    }
 }
 
 export default FeaturedBook
